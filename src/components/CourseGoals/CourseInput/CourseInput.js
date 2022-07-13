ebe556608475d077283732c809631ead86391465
Jonathan Slack <slack6000@gmail.com>
2022-07-13 13:17:09 -0500

    initial commit

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+// import styled from "styled-components";
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
+
+//styled components and dynamic props
+// const FormControl = styled.div`
+//     margin: 0.5rem 0;
+  
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : 'black'};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc' };
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+   //Dynamic classes with css modules
+  return (
+     <form onSubmit={formSubmitHandler}>
+       <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+         <label>Course Goal</label>
+         <input type="text" onChange={goalInputChangeHandler} />
+       </div>
+       <Button type="submit">Add Goal</Button>
+     </form>
+   );
+  
+  //Dynamic Props
+  // return (
+  //   <form onSubmit={formSubmitHandler}>
+  //     <FormControl invalid={!isValid}>
+  //       <label>Course Goal</label>
+  //       <input type="text" onChange={goalInputChangeHandler} />
+  //     </FormControl>
+  //     <Button type="submit">Add Goal</Button>
+  //   </form>
+  // );
+  // return (
+  //   <form onSubmit={formSubmitHandler}>
+  //     <div className="form-control">
+  //       {/* inline styling */}
+  //       <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+  //       <input style={{
+  //         borderColor: !isValid ? 'red' : 'black',
+  //         background: !isValid ? 'salmon' : 'transparent'
+  //       }}
+  //         type="text" onChange={goalInputChangeHandler} />
+  //     </div>
+  //     <Button type="submit">Add Goal</Button>
+  //   </form>
+  // );
+};
+
+export default CourseInput;
